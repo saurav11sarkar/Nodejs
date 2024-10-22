8f63b1b9dc5090107b6587b18ffc22f07dc08aaa
saurav11sarkar <sarkarsaurav11@outlook.com>
2024-10-22 08:32:44 +0600

    nodejs-20.1

--- nodejs-20.1/app.js
+++ nodejs-20.1/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const USER = require('./models/user.model');
 const router = require('./router/user.router');
 require('./models/db.model')
 
